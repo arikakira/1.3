95aa66b08261cce14b631a76b06afe5518fbd62a
arikakira <samanthal242@nycstudents.net>
2024-09-15 00:27:31 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welome to my calculator!");
+        System.out.println("Enter your first number.");
+        Scanner s = new Scanner(System.in);
+        int first = s.nextInt();
+        System.out.println("What operation would you like to do?");
+        String operation = s.next();
+        System.out.println("Enter your second number.");
+        int second = s.nextInt();
+        if(operation.equals("+"))
+        {
+            System.out.println("Answer: " + (first + second));
+        } else if(operation.equals("-")) {
+            System.out.println("Answer: " + (first - second));
+        } else if(operation.equals("*")) {
+            System.out.println("Answer: " + (first * second));
+        } else if(operation.equals("/")) {
+            if(second == 0) {
+            System.out.println("Cannot divide by zero");
+            } else {
+                System.out.println("Answer: " + (first / second));
+            }
+        }
+        s.close();
+    }
+}
